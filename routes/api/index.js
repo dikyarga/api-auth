59fa1a824c88327535798e6a7d44cb0dec92197e
Diky Arga <dikyarga.id@gmail.com>
2017-02-27 16:20:44 +0700

    add login register logic + generated token api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var router = express.Router();
-let base64 = require('base-64');
-const crypto = require('crypto');
 
-// call the models
-let db = require('../../models')
+let authController = require('../../controllers/auth')
+
+router.post('/signup', authController.signup)
+
+router.post('/signin', authController.signin)
+
 
 /* GET users listing. */
 router.get('/hello', function(req, res, next) {
   res.send('Print hello, ' + req.query.name + ' !')
 });
-
-router.post('/signup', function(req, res, next){
-  
-})
 
 module.exports = router;
