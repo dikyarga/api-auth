806b2f24192d04257e9745635f72ee26d65d251a
Diky Arga <dikyarga.id@gmail.com>
2017-02-27 16:27:17 +0700

    add auth helper for middleware in users routes

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var authHelper = require('./helpers/auth')
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var usersAPI = require('./routes/api/users');
@@ -26,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/api/users', usersAPI);
+app.use('/api/users', authHelper, usersAPI);
 app.use('/api/', indexAPI);
 
 // catch 404 and forward to error handler
